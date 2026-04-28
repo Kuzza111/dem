ad1fb22023f1cfa9fee0dc2b7aad46e3a70931a6
Kuzza111 <zhelankin06@bk.ru>
2026-04-28 14:27:02 +0300

    Добавьте файлы проекта.

---
+++ forms1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace forms1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // label click does nothing
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Guest mode: open Form2 without authentication
+            var form2 = new Form2();
+            form2.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectString = @"Data Source=.\SQLEXPRESS;Initial Catalog=MyDatabase;Integrated Security=True";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectString))
+                {
+                    connection.Open();
+
+                    string sql = "SELECT COUNT(1) FROM Users WHERE [Логин] = @login AND [Пароль] = @password";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@login", textBox1.Text.Trim());
+                        cmd.Parameters.AddWithValue("@password", textBox2.Text);
+
+                        int count = Convert.ToInt32(cmd.ExecuteScalar());
+                        if (count >0)
+                        {
+                            // credentials match — open second form
+                            var form2 = new Form2();
+                            form2.Show();
+                            this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Неверный логин или пароль", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при подключении к базе: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
